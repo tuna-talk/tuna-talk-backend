7b6694528a3db2d7a806cfbe85659186d1c4ba69
kwon woo <dirn0568@naver.com>
2023-01-25 14:11:29 +0900

    chat socket

---
+++ src/main/java/shop/iamhyunjun/tunatalk/controller/ChatController.java
@@ -0,0 +1,19 @@
+package shop.iamhyunjun.tunatalk.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+import shop.iamhyunjun.tunatalk.dto.ChatMessageDto;
+
+@RequiredArgsConstructor
+@Controller
+public class ChatController {
+    private final SimpMessageSendingOperations messagingTemplate;
+    @ResponseBody
+    @MessageMapping("/chat/message") // socket 통신은 request를 안주나???
+    public void message(ChatMessageDto chatMessageDto) {
+        messagingTemplate.convertAndSend("/sub/chat/room/" + chatMessageDto.getRoomId(), chatMessageDto); // + roomId 있었음
+    }
+}
---
+++ src/main/java/shop/iamhyunjun/tunatalk/dto/ChatMessageDto.java
@@ -0,0 +1,13 @@
+package shop.iamhyunjun.tunatalk.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+// @Setter 이거 없애니까 message, userJWT roomUUID가 회색빛으로 바뀜
+@NoArgsConstructor
+public class ChatMessageDto {
+    private Long roomId;
+    private String userName;
+    private String message; // 메시지
+}
