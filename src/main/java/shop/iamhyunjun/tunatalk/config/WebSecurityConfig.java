0bcc5ce88c9a85e7225a552d9ba8dddfe61d0f0e
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-25 19:09:20 +0900

    update

--- src/main/java/shop/iamhyunjun/tunatalk/config/WebSecurityConfig.java
+++ src/main/java/shop/iamhyunjun/tunatalk/config/WebSecurityConfig.java
@@ -45,6 +45,7 @@ public class WebSecurityConfig {
 
         http.authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
+                .antMatchers("/").permitAll()
 //                .requestMatchers("/api/search").permitAll()
 //                .requestMatchers("/api/shop").permitAll()
                 .anyRequest().authenticated()
