0cea3ccecd895c445faa20980741b2ff404cce4d
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-25 12:21:42 +0900

    initial commit

---
+++ src/main/java/shop/iamhyunjun/tunatalk/TunaTalkApplication.java
@@ -0,0 +1,13 @@
+package shop.iamhyunjun.tunatalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TunaTalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TunaTalkApplication.class, args);
+    }
+
+}
