88b751941a23d1fe2853f290184b2d8169d61397
hyunjunhwang1994 <hyunjunhwang1994@gmail.com>
2023-01-25 18:54:28 +0900

    test controller

---
+++ src/main/java/shop/iamhyunjun/tunatalk/controller/HjTestController.java
@@ -0,0 +1,29 @@
+package shop.iamhyunjun.tunatalk.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class HjTestController {
+    private final Environment env;
+
+    @GetMapping("/")
+    public String gyunny() {
+        List<String> profile = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfiles = Arrays.asList("real1", "real2");
+        String defaultProfile = profile.isEmpty() ? "default" : profile.get(0);
+
+        return profile.stream()
+                .filter(realProfiles::contains)
+                .findAny()
+                .orElse(defaultProfile);
+    }
+
+}
